376d4eb5739e134eb4b8eb41c0e0184ab0da63ec
Priyanshi Bhikadiya <110802403+priyabhikadiya@users.noreply.github.com>
2023-06-24 20:30:19 +0530

    Initial Commit

---
+++ Personal Notes/Personal_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_NotesApp.swift
+//  Personal Notes
+//
+//  Created by SARVADHI on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Personal_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
